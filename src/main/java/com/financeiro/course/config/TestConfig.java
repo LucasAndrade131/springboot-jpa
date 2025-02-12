49b0f83b8c9ea4fb65ce0c1f2b00c45597419698
sbm-landrade <lucas.andrade@sbmtechnology.com.br>
2025-02-11 22:39:04 -0300

    Entidade Category

--- src/main/java/com/financeiro/course/config/TestConfig.java
+++ src/main/java/com/financeiro/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.financeiro.course.config;
 
+import com.financeiro.course.entities.Category;
 import com.financeiro.course.entities.Order;
 import com.financeiro.course.entities.User;
 import com.financeiro.course.entities.enums.OrderStatus;
+import com.financeiro.course.repositories.CategoryRepository;
 import com.financeiro.course.repositories.OrderRepository;
 import com.financeiro.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired //Injeção de dependência
     private OrderRepository orderRepository;
 
+    @Autowired //Injeção de dependência
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        //classe idependente para popular o banco de dados
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
