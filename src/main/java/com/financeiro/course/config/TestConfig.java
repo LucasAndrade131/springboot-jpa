02a0b9a75f0387645689fd79fc8aec5e5f99290b
sbm-landrade <lucas.andrade@sbmtechnology.com.br>
2025-02-17 22:50:54 -0300

    Entidade OrderItem. Associação muitos-para-muitos com dados extras - PARTE 2

--- src/main/java/com/financeiro/course/config/TestConfig.java
+++ src/main/java/com/financeiro/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.financeiro.course.config;
 
-import com.financeiro.course.entities.Category;
-import com.financeiro.course.entities.Order;
-import com.financeiro.course.entities.Product;
-import com.financeiro.course.entities.User;
+import com.financeiro.course.entities.*;
 import com.financeiro.course.entities.enums.OrderStatus;
-import com.financeiro.course.repositories.CategoryRepository;
-import com.financeiro.course.repositories.OrderRepository;
-import com.financeiro.course.repositories.UserRepository;
-import com.financeiro.course.repositories.ProductRepository;
+import com.financeiro.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired //Injeção de dependência
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -67,5 +64,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
--- src/main/java/com/financeiro/course/entities/OrderItem.java
+++ src/main/java/com/financeiro/course/entities/OrderItem.java
@@ -1,5 +1,6 @@
 package com.financeiro.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.financeiro.course.entities.pk.OrderItemPK;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -15,7 +16,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -39,6 +40,7 @@ public class OrderItem implements Serializable {
         id.setProduct(product);
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
