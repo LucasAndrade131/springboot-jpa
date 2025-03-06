84f21db5ff6a8bfbd9812506ecb53bb1113ce382
sbm-landrade <lucas.andrade@sbmtechnology.com.br>
2025-03-06 00:49:10 -0300

    Tratamento de exceção - update

--- src/main/java/com/financeiro/course/services/UserService.java
+++ src/main/java/com/financeiro/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.financeiro.course.entities.User;
 import com.financeiro.course.repositories.UserRepository;
 import com.financeiro.course.services.exceptions.DatabaseException;
 import com.financeiro.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -32,7 +33,7 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        try{
+        try {
             repository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id); // Lança uma exceção para manter a consistência
+        }
     }
 
     private void updateData(User entity, User obj) {
